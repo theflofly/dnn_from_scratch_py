1c59d7f41c4ab5b6c4508e0081243eb55ea9a6b8
Florian Courtial <floriancourtial@gmail.com>
2017-04-06 11:06:18 +0200

    Adding comments.

--- predict.py
+++ predict.py
@@ -6,6 +6,9 @@ import numpy as np
 
 
 class Predict:
+    """
+    We save the data set metadata.
+    """
     def __init__(self, mean_km, std_km, mean_age, std_age, min_price, max_price):
         self.mean_km = mean_km
         self.std_km = std_km
@@ -14,6 +17,9 @@ class Predict:
         self.min_price = min_price
         self.max_price = max_price
 
+    """
+    This method returns the car's data normalized using the data set metadata.
+    """
     def input(self, km, fuel, age):
         km = (km - self.mean_km) / self.std_km
         fuel = -1 if fuel == 'Diesel' else 1
@@ -22,5 +28,9 @@ class Predict:
             km, fuel, age
         ]])
 
+    """
+    This method returns the price in euro from the output of the network. The inverse of the standardize process for the
+    price is applied.
+    """
     def output(self, price):
         return price * (self.max_price - self.min_price) + self.min_price
